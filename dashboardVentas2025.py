1359e3ca6aa57fb67b1fc133ca3027487e8625bc
lizethyeh <lizethyeh@gmail.com>
2025-10-15 19:24:17 -0600

    Update dashboardVentas2025.py

--- dashboardVentas2025.py
+++ dashboardVentas2025.py
@@ -13,16 +13,16 @@ total_sales_per_product = df_order_central.groupby('Product Name')['Sales'].sum(
 sorted_products_by_sales = total_sales_per_product.sort_values(ascending=False)
 top_5_products = sorted_products_by_sales.head(5)
 
+# Manually wrap product names for sales chart
+wrapped_sales_product_names = top_5_products.index.str.wrap(20)
+
 # Create sales bar chart
 fig_sales = px.bar(top_5_products,
-                   x=top_5_products.index,
+                   x=wrapped_sales_product_names,
                    y=top_5_products.values,
                    title='Top 5 Selling Products by Sales',
                    labels={'x': 'Product Name', 'y': 'Total Sales'})
-fig_sales.update_layout(xaxis = dict(tickangle = -45,
-                                tickfont = dict(size=10),
-                                automargin=True),
-                  xaxis_tickformat = '<br>'.join(['%s' % i for i in top_5_products.index.str.wrap(20)])) # Apply wrapping to sales chart
+fig_sales.update_layout(xaxis_tickangle=-45)
 
 st.plotly_chart(fig_sales)
 
@@ -31,16 +31,17 @@ total_profit_per_product = df_order_central.groupby('Product Name')['Profit'].su
 sorted_products_by_profit = total_profit_per_product.sort_values(ascending=False)
 top_5_products_by_profit = sorted_products_by_profit.head(5)
 
+# Manually wrap product names for profit chart
+wrapped_profit_product_names = top_5_products_by_profit.index.str.wrap(20)
+
+
 # Create profit bar chart
 fig_profit = px.bar(top_5_products_by_profit,
-                    x=top_5_products_by_profit.index,  # Corrected typo here
+                    x=wrapped_profit_product_names, # Use wrapped names here
                     y=top_5_products_by_profit.values,
                     title='Top 5 Most Profitable Products',
                     labels={'x': 'Product Name', 'y': 'Total Profit'})
 
-fig_profit.update_layout(xaxis = dict(tickangle = -45,
-                                tickfont = dict(size=10),
-                                automargin=True),
-                  xaxis_tickformat = '<br>'.join(['%s' % i for i in top_5_products_by_profit.index.str.wrap(20)])) # Apply wrapping to profit chart
+fig_profit.update_layout(xaxis_tickangle=-45) # No need for xaxis_tickformat with manual wrapping
 
 st.plotly_chart(fig_profit)
