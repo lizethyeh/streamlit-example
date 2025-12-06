24e4625893484522f1d9a4fa2cfb2f65d3d7641a
lizethyeh <lizethyeh@gmail.com>
2025-12-05 20:56:00 -0600

    Update Dashboard Delitos en Yucatánapp.py

--- Dashboard Delitos en Yucatánapp.py
+++ Dashboard Delitos en Yucatánapp.py
@@ -338,14 +338,15 @@ else:
         )
 
         st.pydeck_chart(pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state=view_state,
-            layers=[geojson_layer],
-            tooltip=tooltip={
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state=view_state,
+    layers=[geojson_layer],
+    tooltip={
         "html": "<b>Municipio:</b> {properties.municipio_nombre}<br/>"
                 "<b>Total de Incidentes:</b> {properties.incidentes_total}"
     }
 ))
+
     else:
         st.info("No hay datos para mostrar en el mapa con los filtros seleccionados.")
 
